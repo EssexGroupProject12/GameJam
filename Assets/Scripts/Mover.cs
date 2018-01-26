250554909d22e29a179fa64cd35800ccbfce4c6f
Tomas Oplatek <oplatek.t@gmail.com>
2018-01-26 21:24:49 +0000

    Added folder structure, added test sprites for objects and player

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    public float Speed;
+
+    private void Start()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(Speed, 0);
+    }
+}
