2a3bd11f8294046c583b3490ee4117fa35738870
Munro <jm17913@essex.ac.uk>
2018-01-27 10:32:04 +0000

    Added gameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+
+    public GameObject[] Collectables = new GameObject[2];
+    public GameObject Chosen;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            Chosen = Collectables[Random.Range(0, Collectables.Length)];
+        }
+    }
+}
