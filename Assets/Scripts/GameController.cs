a75c61e585cabc03757b4c83e66ab523ab7f8429
Munro <jm17913@essex.ac.uk>
2018-01-27 10:50:07 +0000

    Manual merge fix.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,9 @@ public class GameController : MonoBehaviour
     public int Score;
     public Text ScoreText;
 
+    public GameObject[] Collectables = new GameObject[2];
+    public GameObject Chosen;
+
     private void Start()
     {
         
@@ -16,5 +19,11 @@ public class GameController : MonoBehaviour
     private void Update()
     {
         ScoreText.text = string.Format("Score: {0}", Score);
+    }
+
+    private IEnumerator Spawn()
+    {
+            yield return new WaitForSeconds(2);
+            Chosen = Collectables[Random.Range(0, Collectables.Length)];
     }
 }
