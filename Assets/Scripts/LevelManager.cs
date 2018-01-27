79c8ace651b6757b9bf8576aee5c10407875c6d0
Tomas Oplatek <oplatek.t@gmail.com>
2018-01-27 17:12:47 +0000

    Removed fading effects

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,25 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    private FaderScript FaderScript;
     private void Start()
     {
-        FaderScript = GetComponent<FaderScript>();
     }
     public void LoadScene(string name)
     {
-        StartCoroutine(FadeOut(name));
-    }
-
-    public void QuitGame()
-    {
-        StartCoroutine(FadeOut(null));
-    }
-
-    private IEnumerator FadeOut(string name)
-    {
-        var howLongToWait = FaderScript.BeginFade(1);
-        yield return new WaitForSeconds(howLongToWait);
         if (string.IsNullOrEmpty(name))
         {
             Application.Quit();
@@ -32,5 +18,10 @@ public class LevelManager : MonoBehaviour
         {
             SceneManager.LoadScene(name);
         }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
