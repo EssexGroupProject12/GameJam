0faabbdbd60a4855c871b5c5ca19e2439474834a
Roger <renyifan008@gmail.com>
2018-01-27 09:17:53 +0000

    Add boundary

---
+++ Assets/Scripts/DestoryBybound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryBybound : MonoBehaviour {
+
+	void  OnTriggerEnter2D(Collider2D coll){
+		//Debug.Log("touch");
+		if (coll.gameObject.tag == "boundary") {
+			Destroy (gameObject);
+		}
+     }
+}
