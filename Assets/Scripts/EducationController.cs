27dc86bd56de4c25b28887176d48dc8f553bbd54
Tomas Oplatek <oplatek.t@gmail.com>
2018-03-18 16:04:10 +0000

    Education scene can now be skipped by pressing Enter or Space

---
+++ Assets/Scripts/EducationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EducationController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+}
